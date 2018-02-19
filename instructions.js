e61eb8c111dbbee60ad75cf99574a86fc67943fd
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-18 16:36:49 -0800

    Implement basic component and server

---
+++ instructions.js
@@ -0,0 +1,42 @@
+// Assignment
+// ----------
+
+// Your assignment is to build a full-stack simple version of Youtube using the public Youtube API at https://developers.google.com/youtube/v3/getting-started
+// It doesn't have to look exactly like the actual Youtube, but the picture is provided to give you a general idea of layout. 
+
+// What We're Looking For:
+
+// 1. React (with or without Redux)
+// 2. Server Side Rendering
+// 3. Database Connection
+// 4. Deployment (Heroku, AWS, etc.)
+
+// I. Search Bar
+    
+//     * Standard search bar to look up a video 
+
+// II. Main Video Section
+    
+//     * Should be able to play and pause the video
+//     * Underneath the video:
+//         * Title
+//             * Doesn't have to be clickable
+//         * Description under title
+//         * List the 'Like' and 'Dislike' counts
+//             * You don't have to have a functional 'like' button, just list how many likes and dislikes the video received
+
+// III. Comments Section 
+    
+//     * Render a list of the top comments with the associated users
+
+
+// IV. Videos List
+
+//     * Render a clickable list of videos with image thumbnails, similar to the related videos section of Youtube
+//         * Each small video should have their clickable title next to it
+//     * When you click a video, it should appear in the main section as the main video 
+//     * The list should update and still show a full list of videos 
+
+// V. Database
+    
+//     * Save a list of the clicked videos to a database (not the actual video files, just the title names)
---
+++ server/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var items = require('../database-mongo');
+
+var app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+app.get('/items', function (req, res) {
+  items.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log('listening on port', port);
+});
+
