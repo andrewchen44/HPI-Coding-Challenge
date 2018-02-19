042be2eb25f1414341c7adb27cf6ce45a1ef85dc
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-19 14:13:34 -0800

    Format search bar

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div>search bar goes here</div>;
+  return <div className="search-bar form-inline">
+      <input className="form-control" type="text" />
+      <button className="btn hidden-sm-down">
+        <span className="glyphicon glyphicon-search" />
+      </button>
+    </div>;
 };
 
 export default SearchBar;
